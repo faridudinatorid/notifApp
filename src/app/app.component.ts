f651047f32dc99c71075e8a344bec0e2689b8eac
faridudinmuhammad <xxxx@xFARID-MacBook-Pro.local>
2019-11-21 14:15:40 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +23,16 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+  
+      window["plugins"].OneSignal
+        .startInit("d6728c2a-ff71-4686-9444-65ad777b8a29", "853577844207")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
+
     });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,33 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//koneksi firebase
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    // apiKey: "AIzaSyA0uzu6bKzrTPN9IVuDbyu2LLk6DzBPfWg",
+    // authDomain: "ionicpush-72733.firebaseapp.com",
+    // databaseURL: "https://ionicpush-72733.firebaseio.com",
+    // projectId: "ionicpush-72733",
+    // storageBucket: "ionicpush-72733.appspot.com",
+    // messagingSenderId: "853577844207"
+    
+    
+
+    apiKey: "AIzaSyDFF4BzF97NATzK7t1uu3T4TvaQ6oBDjlo",
+    authDomain: "data-mahasiswa-724355.firebaseapp.com",
+    databaseURL: "https://data-mahasiswa-724355.firebaseio.com",
+    projectId: "data-mahasiswa-724355",
+    storageBucket: "data-mahasiswa-724355.appspot.com",
+    messagingSenderId: "677379842119",
+    
+    // apiKey: "AIzaSyBmAWxMhNS6YE-FdX7Wnq_xS-HGRdbh46w",
+    // authDomain: "latihanfirebase-1781a.firebaseapp.com",
+    // databaseURL: "https://latihanfirebase-1781a.firebaseio.com",
+    // projectId: "latihanfirebase-1781a",
+    // storageBucket: "latihanfirebase-1781a.appspot.com",
+    // messagingSenderId: "524349726838"
+  }
 };
 
 /*
